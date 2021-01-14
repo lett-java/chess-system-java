7daa78c6f811c143ea5080b26f061a1b8a535549
Fabio Lettieri <fabiolettieri09@gmail.com>
2021-01-14 13:36:05 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.ColorEnum;
 
@@ -18,7 +19,80 @@ public class King extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+		
+		moveAbove(mat, p);
+		moveBelow(mat, p);
+		moveLeft(mat, p);
+		moveRight(mat, p);
+		moveNw(mat, p);
+		moveNe(mat, p);
+		moveSw(mat, p);
+		moveSe(mat, p);
+		
 		return mat;
+	}
+
+	private void moveSe(boolean[][] mat, Position p) {
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveSw(boolean[][] mat, Position p) {
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveNe(boolean[][] mat, Position p) {
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveNw(boolean[][] mat, Position p) {
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveRight(boolean[][] mat, Position p) {
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveLeft(boolean[][] mat, Position p) {
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveBelow(boolean[][] mat, Position p) {
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+
+	private void moveAbove(boolean[][] mat, Position p) {
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
 	}
 
 }
